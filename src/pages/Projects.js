33510b19342edd29d29ec07220335910c65f07f2
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-15 14:06:45 -0700

    fix footer on projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -26,22 +26,25 @@ function Projects(props) {
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
-    return (<div className="projects md:flex md:flex-wrap">
-      {projects.map((project) => (
-      <div className="card mb-8 mx-auto md:mx-[1rem]">
-        <img src={project.image} alt={project.name} className="card-img"/>
-          <p className="card-title">{project.name}</p>
-          <p className="list-group-item text-xl">{project.technologies}</p>
-        <div className="card-body">
-          <a href={project.git} className="card-link text-xl">
-            GitHub
-          </a>
-          <a href={project.live} className="card-link text-xl">
-            Live Site
-          </a>
+    return (
+    <div>
+      <div className="projects md:flex md:flex-wrap">
+        {projects.map((project) => (
+        <div className="card mb-8 mx-auto md:mx-[1rem]">
+          <img src={project.image} alt={project.name} className="card-img"/>
+            <p className="card-title">{project.name}</p>
+            <p className="list-group-item text-xl">{project.technologies}</p>
+          <div className="card-body">
+            <a href={project.git} className="card-link text-xl">
+              GitHub
+            </a>
+            <a href={project.live} className="card-link text-xl">
+              Live Site
+            </a>
+          </div>
         </div>
+      ))}
       </div>
-    ))}
       <Footer/>
     </div>)
   };
