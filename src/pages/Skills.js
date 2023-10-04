e6f0a1ba1c390aae82a3da445a5fb8f8fb9b9d2f
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-10-04 10:30:38 -0700

    edit top margin for skills card

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer/Footer";
 function Skills() {
     return (
         <div>
-            <div className="text-[#00332c] max-w-max md:mx-auto p-5 mx-4 mt-[3rem] border-[2px] border-[#00332c] border-dashed">
+            <div className="text-[#00332c] max-w-max md:mx-auto p-5 mx-4 mt-[3rem] md:mt-[3.75rem] xl:mt-[4.75rem] border-[2px] border-[#00332c] border-dashed">
                 <h1 className="text-[1.75rem] text-center px-4 w-max bg-[rgba(250,174,43,0.8)]">Skills:</h1>
                 <h3 className="text-[1.5rem] underline underline-offset-4 decoration-1">Languages:</h3>
                 <p className="text-[1.5rem]">JavaScript, Python, HTML, CSS, SQL, Mongo, JSON, EJS, DTL</p>
